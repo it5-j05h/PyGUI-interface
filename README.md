68fa0280b0c705c51a94a68bf33ccb806bb9f988
Joshua <joshua.rogers.2105@gmail.com>
2022-11-07 08:16:11 +0000

    System Finished
    proof of concept idea finished, all functionality working.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PyGUI interface
+
+# Simple proof of concept for a Main-Menu system
  
--- app.py
+++ app.py
@@ -5,10 +5,18 @@ import os
 root = tk.Tk()
 apps = []
 
-
+if os.path.isfile('save.txt'):
+    with open('save.txt', 'r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]
 
 
 def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
     filename= filedialog.askopenfilename(initialdir="/", title="Select File",filetypes=(
                                                                                         ("executables","*.exe"), ("all files","*.*")))
     apps.append(filename)
@@ -17,10 +25,12 @@ def addApp():
         label = tk.Label(frame, text=app, bg="grey")
         label.pack()
 
+def runApps():
+    for app in apps:
+        os.startfile(app)
 
 
-
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas = tk.Canvas(root, height=400, width=350, bg="#263D42")
 canvas.pack()
 
 frame = tk.Frame(root, bg="white")
@@ -29,8 +39,16 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#263D42", command=addApp)
 openFile.pack()
 
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42")
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#263D42", command=runApps)
 runApps.pack()
 
 
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
 root.mainloop()
+
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
